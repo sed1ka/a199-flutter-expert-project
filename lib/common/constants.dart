6d993ff52a390d5798132d1cdcfc37f37c1022c6
Septian Dika <83001459+sed1ka@users.noreply.github.com>
2026-06-19 00:56:22 +0700

    Update constants.dart

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -18,6 +18,8 @@ final TextStyle kHeading6 = GoogleFonts.poppins(
     fontSize: 19, fontWeight: FontWeight.w500, letterSpacing: 0.15);
 final TextStyle kSubtitle = GoogleFonts.poppins(
     fontSize: 15, fontWeight: FontWeight.w400, letterSpacing: 0.15);
+final TextStyle kSubtitle2 = GoogleFonts.poppins(
+    fontSize: 15, fontWeight: FontWeight.w600, letterSpacing: 0.15);
 final TextStyle kBodyText = GoogleFonts.poppins(
     fontSize: 13, fontWeight: FontWeight.w400, letterSpacing: 0.25);
 

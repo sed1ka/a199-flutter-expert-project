2cdcd15af542f5746de8f2a72d87121e83d7ab3d
Septian Dika <83001459+sed1ka@users.noreply.github.com>
2026-07-02 21:13:43 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -62,8 +62,8 @@ Future<void> bootstrap({
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
